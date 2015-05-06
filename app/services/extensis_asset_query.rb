dd441bbc898092691b6f50e725c78df11829a353
Asger Behncke Jacobsen <asger@8kilo.com>
2015-05-06 19:01:17 +0200

    update

--- app/services/extensis_asset_query.rb
+++ app/services/extensis_asset_query.rb
@@ -1,22 +1,21 @@
 class ExtensisAssetQuery
 
-  def initialize(gallery_id, query_term, sort_options)
+  # gallery_id String
+  # query_term ExtensisAssetQuery
+  # query_term ExtensisAssetQuery
+
+  def initialize(gallery_id="", query_term="", sort_options="")
     @gallery_id = gallery_id
     @query_term = query_term
     @sort_options = sort_options
   end
 
-  def to_s
-    builder = Builder::XmlMarkup.new
-    builder.instruct!(:xml, encoding: "UTF-8")
-
-    builder.asset_query { |b|
-      b.gallery_id(@gallery_id) if @gallery_id
-      b.query_term(@query_term) if @query_term
-      b.sort_options(@sort_options) if @sort_options
+  def to_hash
+    {
+      gallery_id: @gallery_id,
+      query_term: @query_term,
+      sort_options: @sort_options
     }
-
-    builder
   end
 
 end
--- app/services/extensis_source_image.rb
+++ app/services/extensis_source_image.rb
@@ -6,15 +6,21 @@ class ExtensisSourceImage
     @source = source
   end
 
-  def to_s
-    builder = Builder::XmlMarkup.new
-    builder.instruct!(:xml, encoding: "UTF-8")
-
-    builder.source_image { |b|
-      b.source_image(@source)
+  def to_hash
+    {
+      source: @source
     }
-
-    builder
   end
+
+  # def to_s
+  #   builder = Builder::XmlMarkup.new
+  #   builder.instruct!(:xml, encoding: "UTF-8")
+
+  #   builder.source_image { |b|
+  #     b.source_image(@source)
+  #   }
+
+  #   builder
+  # end
 
 end
