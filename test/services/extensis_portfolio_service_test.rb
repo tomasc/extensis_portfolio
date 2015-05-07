9a7fc22cc1736e1bc3f2e45a81f87c60d41831ce
Asger Behncke Jacobsen <asger@8kilo.com>
2015-05-07 14:06:41 +0200

    clear it up

--- test/services/extensis_portfolio_service_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ExtensisPortfolioService < ActiveSupport::TestCase
-  test 'it returns the available operations' do
-    extensis_portfolio_service = ExtensisPortfolioService.new
-
-    assert_not_nil extensis_portfolio_service.get_soap_operations, 'Should return soap operations'
-  end
-end
